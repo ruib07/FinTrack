453deb97cb049e9ae3d4632161a8d4f85d472d0b
Rui Barreto <ruibarreto07@outlook.com>
2025-04-11 15:13:27 +0100

    Mobile app not found screen with translation implemented.

--- fintrack.mobile/app/+not-found.tsx
+++ fintrack.mobile/app/+not-found.tsx
@@ -1,16 +1,19 @@
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { Link, Stack } from "expo-router";
+import { useTranslation } from "react-i18next";
 import { StyleSheet } from "react-native";
 
 export default function NotFoundScreen() {
+  const { t } = useTranslation();
+
   return (
     <>
       <Stack.Screen options={{ title: "Oops!" }} />
       <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
+        <ThemedText type="title">{t("screenDontExist")}</ThemedText>
         <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
+          <ThemedText type="link">{t("goToHome")}</ThemedText>
         </Link>
       </ThemedView>
     </>
